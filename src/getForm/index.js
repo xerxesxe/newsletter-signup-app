bcf7c672eb63687517b4a8fecfe1dcd54afe78ab
xerxesxe <jonas.hettwer@gmail.com>
2019-07-08 11:39:50 +0200

    added Function Form

--- src/getForm/index.js
+++ src/getForm/index.js
@@ -1,6 +1,26 @@
-exports.handler = async (event, context) => {
-  // Log the event argument for debugging and for use in local development.
-  console.log(JSON.stringify(event, undefined, 2));
+const formHTML = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Newsletter Sign Up</title>
+  </head>
+  <body>
+    <h1>Subscribe to our newsletter</h1>
+    <form action="submit" method="post">
+        <label>Email
+            <input name="email" type="text">
+        </label>
+        <input type="submit" value="Submit">
+    </form>
 
-  return {};
+  </body>
+</html>`;
+exports.handler = async message => {
+  console.log(message);
+
+  return {
+    statusCode: 200,
+    headers: {'Content-Type': 'text/html'},
+    body: formHTML
+  };
 };
